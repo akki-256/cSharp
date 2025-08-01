19e606d839861160d609d82d9671328088def557
akki-256 <168257172+akki-256@users.noreply.github.com>
2025-08-01 20:12:29 +0900

    初期

---
+++ lesson2/lesson2-1/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int x;
+        bool isInteger = int.TryParse(input, out x);
+        if (isInteger)
+        {
+            x = int.Parse(input);
+            int y = (x > 5) ? 10 : x;
+            Console.WriteLine(y);
+        }
+        else
+        {
+            Console.WriteLine("入力が整数ではありません。");
+        }
+    }
+}
---
+++ lesson2/lesson2-2/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static bool Echo(string message)
+    {
+        Console.WriteLine(message);
+        return true;
+    }
+    static void Main()
+    {
+        Console.WriteLine("ショートサーキットなしの例");
+        var x = Echo("a") | Echo("b");
+        Console.WriteLine("ショートサーキットあり");
+        var y = Echo("c") || Echo("d");
+    }
+}
